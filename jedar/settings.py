83927c99987053827a5e7e571b650bba33c049fb
Inchat <charlespenalosa18@gmail.com>
2020-08-08 00:18:44 +0800

    added heroku

--- jedar/settings.py
+++ jedar/settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku 
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jedar.urls'
@@ -112,11 +116,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),   
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStrorage'
+
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pcharlesirvin@gmail.com'
-EMAIL_HOST_PASSWORD = 'proudmore'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
 
+django_heroku.settings(locals())
